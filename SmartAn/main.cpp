9882815c4074c6767b573bd85ec4a26b59b9ce3b
lengjiexiaozhe <963398110@qq.com>
2021-08-06 20:43:54 -0700

    自适应测试

--- SmartAn/main.cpp
+++ SmartAn/main.cpp
@@ -40,6 +40,10 @@ QVariant GetConfigData(QString qstrnodename, QString qstrkeyname)
 
 int main(int argc, char *argv[])
 {
+#if (QT_VERSION >= QT_VERSION_CHECK(5,9,0))
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
     QApplication a(argc, argv);
 
     QString filePath = QCoreApplication::applicationDirPath();
